c5e7900bafe7a8fe3590387b196a3613ef210857
Rodao Garcia Carmen <carmen.rodao@servexternos.gruposantander.com>
2024-10-04 13:28:41 +0200

    Primera versión de config con la lectura de json en s3

---
+++ src/dataplatform_tools/config.py
@@ -0,0 +1,10 @@
+import boto3
+import json
+
+
+def read_json_config_from_s3(bucket, key):
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    content = response['Body']
+    json_object = json.loads(content.read())
+    return json_object
