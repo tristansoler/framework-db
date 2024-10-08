f775e58027e6ad6d4f80459e439ec1ab431c08eb
x312756-sgt_sangroup <marta.villalbacantero@servexternos.gruposantander.com>
2024-10-08 11:44:15 +0200

    use S3Client in config

--- src/dataplatform_tools/config.py
+++ src/dataplatform_tools/config.py
@@ -1,14 +1,13 @@
-import boto3
 import json
+from src.dataplatform_tools.s3 import S3Client
 
 
-def read_json_config_from_s3(bucket, key):
+def read_json_config_from_s3(s3_client: S3Client, bucket: str, key: str) -> dict:
     try:
-        s3_client = boto3.client('s3')
-        response = s3_client.get_object(Bucket=bucket, Key=key)
-        content = response['Body']
-        json_object = json.loads(content.read())
+        content = s3_client.get_file_content_from_s3(bucket, key)
+        json_object = json.load(content)
         return json_object
     except Exception as error:
         msg_error = f'Error in read_json_config_from_s3: {str(error)}'
+        # TODO: raise specific exception
         raise msg_error
