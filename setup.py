fd7ca4452030f40f59ce7f505a5b504fce280c12
Alejandro Julian Lopez <alejandro.lopezrodriguez@servexternos.gruposantander.com>
2024-10-17 01:52:30 +0200

    Fix

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
-    long_description = f.read()
-
 setup(
    name='data_framework',
    version='1',
    description='Sample',
    license="MIT",
-   long_description=long_description,
    author='WMI',
    author_email='sample@sample',
    packages=['src']
