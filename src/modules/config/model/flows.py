715cfcf55a37d86ef1b669f19e37180d68f3ed70
Alejandro Julian Lopez <alejandro.lopezrodriguez@servexternos.gruposantander.com>
2024-10-16 17:40:12 +0200

    Add more advance in configuration and Storage

---
+++ src/modules/config/model/flows.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+class Enviroment(Enum):
+    LOCAL = "local"
+    REMOTE = "remote"
+
+class DateLocated(Enum):
+    FILENAME = "filename"
+    COLUMN = "column"
+
+class Technologies(Enum):
+    LAMBDA = "lambda"
+    EMR = "emr"
+
+class LandingFileFormat(Enum):
+    CSV = "csv"
+    JSON = "json"
+    EXCEL = "xls"
+
+@dataclass
+class Hardware:
+    ram: int
+    cpu: Optional[int]
+    disk: Optional[int]
+
+@dataclass
+class ProcessingSpecifications:
+    technology: Technologies
+    hardware: Hardware
+
+@dataclass
+class DateLocatedFilename:
+    regex: str
+
+@dataclass
+class CSVSpecs:
+    header_position: int
+    header: bool
+    encoding: str
+    delimiter: str
+    date_located: DateLocated
+    date_located_filename: DateLocatedFilename
+
+@dataclass
+class Parameters:
+    dataflow: str
+    source_file_path: str
+    bucket_prefix: str
+    file_name: str
+    file_date: Optional[str]
+    region: str
+
+
+@dataclass
+class Validations:
+    validate_extension: bool
+    validate_filename: bool
+    validate_csv: bool
+    validate_columns: bool
+
+
+@dataclass
+class IncomingFileLandingToRaw:
+    zipped: str
+    file_format: LandingFileFormat
+    filename_pattern: str
+    ordered_columns: bool
+    csv_specs: CSVSpecs
+    validations: Validations
+
+
+@dataclass
+class Partitions:
+    datadate: bool
+    insert_time: bool
+
+
+@dataclass
+class OutputFile:
+    database: str
+    table: str
+    partitions: Partitions
+
+
+@dataclass
+class LandingToRaw:
+    incoming_file: IncomingFileLandingToRaw
+    output_file: OutputFile
+
+
+@dataclass
+class Flows:
+    landing_to_raw: LandingToRaw
+
+@dataclass
+class Config:
+    flows: Flows
+    environment: Enviroment
+    parameters: Parameters
--- src/modules/storage/s3_storage.py
+++ src/modules/storage/s3_storage.py
@@ -3,27 +3,35 @@ import time
 from botocore.exceptions import ClientError
 from modules.storage.interface_storage import CoreStorageInterface
 from modules.utils.logger import logger
+from modules.config.core import config
+from modules.config.model.flows import Enviroment
+from modules.storage.interface_storage import (
+    CoreStorageInterface,
+    Database,
+    Layer,
+    ReadResponse,
+    WriteResponse
+)
 
 class S3Storage(CoreStorageInterface):
     def __init__(self):
-        
-        self.s3 = boto3.client('s3', region_name="eu-west-1")
-        self.bucket_name = ""
-        logger.info(f'S3Storage initialized with bucket: {self.bucket_name}, region: XX')
 
-    def read(self, path: str):
-    
-        logger.info(f'Reading from S3 path: {path}')
+        self.s3 = boto3.client('s3', region_name=config().parameters.region)
+
+    def read(self, layer: Layer, database: Database, table: str) -> ReadResponse:
+        bucket = self._build_bucket_name(layer=layer)
+        key_path = self._build_s3_key_path(database=database, table=table)
+
         return self._retry(lambda: self._read_object(path))
 
-    def _read_object(self, path: str):
-        
+    def _read_object(self, bucket: str, key_path: str):
         try:
-            response = self.s3.get_object(Bucket=self.bucket_name, Key=path)
-            logger.info(f'Successfully read from path: {path}')
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=key_path)
+            logger.info(f'Successfully read from path: {key_path}')
             return response['Body'].read()
         except ClientError as e:
             logger.error(f'Failed to read: {e}')
+            response = ReadResponse()
             raise
 
     def _retry(self, func, retries=3, backoff=2):
@@ -39,6 +47,13 @@ class S3Storage(CoreStorageInterface):
                 else:
                     logger.error(f'All retry attempts failed operation')
                     raise
+
+    def _build_s3_bucket_name(self, layer: Layer) -> str:
+        return f's3://{config().parameters.bucket_prefix}_{layer.value}'
+    
+    def _build_s3_key_path(self, database: Database, table: str) -> str:
+        return f'{database.value}/{table}/'
+
 
     def write(self, path: str, data):
         
