88efc5abd3f4327c638ee4fb9bfbc9beb7fb79f1
Alejandro Julian Lopez <alejandro.lopezrodriguez@servexternos.gruposantander.com>
2024-10-17 02:06:27 +0200

    [WIP] Setup

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-   name='data_framework',
-   version='1',
-   description='Sample',
-   license="MIT",
-   author='WMI',
-   author_email='sample@sample',
-   packages=['src']
+    name='data_framework',
+    version='1',
+    description='Sample',
+    license="MIT",
+    author='WMI',
+    author_email='sample@sample',
+    python_requires='>=3.9',
+    packages=find_packages(
+        include=['src'],
+        exclude=['tests']
+    )
 )
