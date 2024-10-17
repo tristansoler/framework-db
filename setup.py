a46e0f5542899034266837235af53134da03b5e1
Alejandro Julian Lopez <alejandro.lopezrodriguez@servexternos.gruposantander.com>
2024-10-17 02:33:08 +0200

    [WIP] Setup

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
     author_email='sample@sample',
     python_requires='>=3.9',
     packages=find_packages(
-        include=['src'],
-        exclude=['tests']
-    )
+        where='src',
+        exclude=['tests', 'tests.*']
+    ),
+    package_dir={'': 'src'},
+    include_package_data=True
 )
