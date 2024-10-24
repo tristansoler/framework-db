d1f5b3ee65688f572bf7400303beb4b7aeea2d52
x312756-sgt_sangroup <marta.villalbacantero@servexternos.gruposantander.com>
2024-10-24 11:24:46 +0200

    update inferface

--- src/data_framework/modules/data_process/interface_data_process.py
+++ src/data_framework/modules/data_process/interface_data_process.py
@@ -4,5 +4,15 @@ from abc import ABC, abstractmethod
 class DataProcessInterface(ABC):
 
     @abstractmethod
+    def update(self, flowdata: str, table_name: str):
+        # Abstract class to define the basic storage interface
+        pass
+
+    @abstractmethod
+    def delete(self, df: any, flowdata: str, table_name: str):
+        # Abstract method to write data to a specific location
+        pass
+
+    @abstractmethod
     def merge(self):
         pass
