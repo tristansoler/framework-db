fc38c28674607f6c66d3d7f0e4f14c07cd53ffb7
Alejandro Julian Lopez <alejandro.lopezrodriguez@servexternos.gruposantander.com>
2024-10-29 17:25:35 +0100

    Add properties

--- src/data_framework/dataflow/staging.py
+++ src/data_framework/dataflow/staging.py
@@ -6,47 +6,62 @@ from typing import Any
 
 class RawToStaging:
 
+    @property
+    def origin_dataframe(self):
+        return self.__df
+    
+    @property
+    def config(self):
+        return self.__config
+    
+    @property
+    def logger(self):
+        return self.__logger
+    
+    @property
+    def data_process(self):
+        return self.__data_process
+
     def __init__(self):
-        self.config = config()
-        self.logger = logger
-        self.data_process = CoreDataProcess()
-        self.incoming_file_config = self.config.processes.raw_to_staging.incoming_file
-        self.output_file_config = self.config.processes.raw_to_staging.output_file
-        self.df = self.read()
+        self.__config = config()
+        self.__logger = logger
+        self.__data_process = CoreDataProcess()
+        self.__incoming_file_config = self.__config.processes.raw_to_staging.incoming_file
+        self.__output_file_config = self.__config.processes.raw_to_staging.output_file
+        self.__df = self.read()
 
     def read(self) -> Any:
-        partition = f'{self.incoming_file_config.partition_field}={self.config.parameters.file_date}'
-        input_table = f'{self.incoming_file_config.database_relation}.{self.incoming_file_config.table}'
-        response = self.data_process.datacast(
-            self.incoming_file_config.database_relation,
-            self.incoming_file_config.table,
-            self.output_file_config.database_relation,
-            self.output_file_config.table,
-            self.incoming_file_config.partition_field,
-            self.config.parameters.file_date
+        partition = f'{self.__incoming_file_config.partition_field}={self.__config.parameters.file_date}'
+        input_table = f'{self.__incoming_file_config.database_relation}.{self.__incoming_file_config.table}'
+        response = self.__data_process.datacast(
+            self.__incoming_file_config.database_relation,
+            self.__incoming_file_config.table,
+            self.__output_file_config.database_relation,
+            self.__output_file_config.table,
+            self.__incoming_file_config.partition_field,
+            self.__config.parameters.file_date
         )
         if response.success:
             df = response.data
-            self.logger.info(f'Read {df.count()} rows from {input_table} with partition {partition}')
+            self.__logger.info(f'Read {df.count()} rows from {input_table} with partition {partition}')
             return df
         else:
-            self.logger.error(f'Error reading data from {input_table} with partition {partition}: {response.error}')
+            self.__logger.error(f'Error reading data from {input_table} with partition {partition}: {response.error}')  
 
     def write(self) -> None:
-        output_table = f'{self.output_file_config.database_relation}.{self.output_file_config.table}'
-        response = self.data_process.merge(
-            self.df,
-            self.output_file_config.database_relation,
-            self.output_file_config.table,
+        output_table = f'{self.__output_file_config.database_relation}.{self.__output_file_config.table}'
+        response = self.__data_process.merge(
+            self.__df,
+            self.__output_file_config.database_relation,
+            self.__output_file_config.table,
             # TODO: obtain primary keys from Glue table
-            self.output_file_config.primary_keys
+            self.__output_file_config.primary_keys
         )
         if response.success:
-            self.logger.info(f'Successfully inserted data into {output_table}')
+            self.__logger.info(f'Successfully inserted data into {output_table}')
         else:
-            self.logger.error(f'Error inserting data into {output_table}: {response.error}')
+            self.__logger.error(f'Error inserting data into {output_table}: {response.error}')
 
 
 if __name__ == '__main__':
     staging = RawToStaging()
-    staging.write()
