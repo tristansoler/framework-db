56411a6cb19c2471792245af8a379e0a33d3ab03
x312756-sgt_sangroup <marta.villalbacantero@servexternos.gruposantander.com>
2025-02-03 12:01:11 +0100

    fix bug in apply transformations

--- src/data_framework/modules/data_process/integrations/spark/utils.py
+++ src/data_framework/modules/data_process/integrations/spark/utils.py
@@ -1,6 +1,5 @@
 from data_framework.modules.catalogue import interface_catalogue as catalogue
 from data_framework.modules.config.model.flows import Transformation
-import os
 from importlib import import_module
 from typing import List, Dict, Any
 from pyspark.sql import DataFrame
@@ -23,32 +22,12 @@ def apply_transformations(
     **kwargs: Dict[str, Any]
 ) -> DataFrame:
     for transformation in transformations:
-        module_names = list_transformation_modules(
-            'data_framework.modules.data_process.integrations.spark.transformations'
-        )
-        transformation_function = get_transformation_function(
-            module_names, transformation.type.value
-        )
-        df = transformation_function(df, transformation, **kwargs)
-    return df
-
-
-def list_transformation_modules(package: str) -> List[str]:
-    package_module = import_module(package)
-    modules = [
-        f'{package}.{filename[:-3]}'
-        for filename in os.listdir(package_module.__path__[0])
-        if filename.endswith('.py') and filename != '__init__.py'
-    ]
-    return modules
-
-
-def get_transformation_function(module_names: List[str], function_name: str) -> Any:
-    for module_name in module_names:
         try:
+            function_name = transformation.type.value
+            module_name = f'data_framework.modules.data_process.integrations.spark.transformations.{function_name}'
             module = import_module(module_name)
-            function = getattr(module, function_name)
-            return function
+            transformation_function = getattr(module, function_name)
+            df = transformation_function(df, transformation, **kwargs)
         except (ModuleNotFoundError, AttributeError):
-            pass
-    raise NotImplementedError(f'Transformation {function_name} not implemented')
+            raise NotImplementedError(f'Transformation {function_name} not implemented')
+    return df
