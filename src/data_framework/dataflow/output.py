f670d69cfe2ceaaad16e168f1d3c7d390e948121
x312756-sgt_sangroup <marta.villalbacantero@servexternos.gruposantander.com>
2025-02-05 10:38:07 +0100

    add output notification

--- src/data_framework/dataflow/output.py
+++ src/data_framework/dataflow/output.py
@@ -116,8 +116,8 @@ class ProcessingCoordinator(DataFlowInterface):
                 header=config_output.csv_specs.header,
                 index=config_output.csv_specs.index,
                 encoding=config_output.csv_specs.encoding
-            ) 
-            
+            )
+
         if config_output.json_specs:
             response = Storage.base_layer_path(layer=Layer.OUTPUT)
             tmp_write_path = f"{response.path}/{self.config.project_id}/{output_folder}/tmp/"
@@ -126,14 +126,24 @@ class ProcessingCoordinator(DataFlowInterface):
             tmp_read_path = f"{self.config.project_id}/{output_folder}/tmp/"
             response = Storage.list_files(layer=Layer.OUTPUT, prefix=tmp_read_path)
             path_output_file = next((path for path in response.result if ".json" in path), "")
-            
+
             response = Storage.read(layer=Layer.OUTPUT, key_path=path_output_file)
             file_to_save = BytesIO(response.data)
 
         response = self.storage.write_to_path(Layer.OUTPUT, file_path, file_to_save.getvalue())
         if not response.success:
             raise response.error
-    
+        else:
+            self.notifications.send_notification(
+                notification_name='output_generated',
+                arguments={
+                    'dataflow': self.config.parameters.dataflow,
+                    'process': self.config.parameters.process,
+                    'output_name': config_output.name,
+                    'file_path': file_path
+                }
+            )
+
     @staticmethod
     def parse_output_folder(output_folder: str) -> str:
         return re.sub(
