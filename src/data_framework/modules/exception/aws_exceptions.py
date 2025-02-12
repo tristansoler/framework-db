9b955b2785cc755cd9ca37f1679fb149344f181a
x312756-sgt_sangroup <marta.villalbacantero@servexternos.gruposantander.com>
2025-02-12 12:02:39 +0100

    add catalogue exceptions

--- src/data_framework/modules/exception/aws_exceptions.py
+++ src/data_framework/modules/exception/aws_exceptions.py
@@ -5,3 +5,9 @@ class STSError(DataFrameworkError):
 
     def __init__(self, error_message: str):
         super().__init__(error_message)
+
+
+class GlueError(DataFrameworkError):
+
+    def __init__(self, error_message: str):
+        super().__init__(error_message)
